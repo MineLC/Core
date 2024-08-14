ef117cda209aa66aea63d5826ef36fb1879ad67e
ichocomilk <ichocodev@gmail.com>
2024-08-13 23:39:27 -0300

    Only save if is necesary

--- src/main/java/lc/mine/core/listener/PlayerJoinListener.java
+++ src/main/java/lc/mine/core/listener/PlayerJoinListener.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CompletableFuture;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import lc.mine.core.database.Database;
+import lc.mine.core.database.PlayerData;
 import lc.mine.core.listener.data.EventListener;
 
 public class PlayerJoinListener implements EventListener<PlayerJoinEvent> {
@@ -16,6 +17,11 @@ public class PlayerJoinListener implements EventListener<PlayerJoinEvent> {
     }
 
     public void handle(final PlayerJoinEvent event) {
+        final PlayerData data = database.getCached(event.getPlayer().getUniqueId());
+        if (data == null ||
+            (data.getLcoins() == data.getOldLcoins() && data.getVipPoins() == data.getOldVipPoints())) {
+            return;
+        }
         CompletableFuture.runAsync(() -> database.load(event.getPlayer()));
     }    
 }
